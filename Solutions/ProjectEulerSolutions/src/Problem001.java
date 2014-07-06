a7b904f3ccb33c446643431a217a2a2cf6ffb01d
oofoghlu <oofoghlu@hotmail.com>
2014-07-06 19:44:37 +0100

    Finished problem 1

--- Solutions/ProjectEulerSolutions/src/Problem001.java
+++ Solutions/ProjectEulerSolutions/src/Problem001.java
@@ -1,4 +1,30 @@
+/**
+ * @author oofoghlu
+ * @version 1.0
+ */
+import java.util.ArrayList;
+
 
 public class Problem001 {
-
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Double> multiples3or5 = new ArrayList<Double>();
+		
+		//checking all numbers below 1000
+		for (double i = 1; i < 1000; i++) {
+			
+			//checking if number is multiple of 3 or 5
+			if ((i / 5) % 1 == 0 || (i / 3) % 1 == 0)
+				multiples3or5.add(i);
+		}
+		
+		int total = 0;
+		
+		for (double num : multiples3or5) {
+			total += num;
+		}
+		
+		System.out.println("The sum of all the multiples of 3 or 5 below 1000 is: " + total);
+	}
 }
